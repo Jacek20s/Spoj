a131389b3b2b765a130695450230ab5f056cd68c
Jacek20s <jacek20s@o2.pl>
2022-02-05 22:57:11 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a=int.Parse(Console.ReadLine());
+            string b;
+            int c;
+            string[] tablica = new string[a];
+
+            for (int i = 0; i < a; i++)
+            {
+                b = Console.ReadLine();
+                c = b.Length / 2;
+                tablica[i] = b.Substring(0,c);
+            }
+            for (int j = 0; j < tablica.Length; j++)
+            {
+                Console.WriteLine(tablica[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
